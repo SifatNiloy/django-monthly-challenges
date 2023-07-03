990b89cb52faef495def857f4c90407c391fa944
SifatNiloy <sifatniloy18@gmail.com>
2023-07-03 21:37:41 +0600

    {% %} {{}}

--- challenges/views.py
+++ challenges/views.py
@@ -20,16 +20,12 @@ monthly_challenges = {
 }
 
 
-def index(request):
-    list_items = ""
+def index(request):  
     months = list(monthly_challenges.keys())
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
 
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    return render(request, "challenges/index.html",{
+        "months":months
+    })
 
 
 def monthly_challenges_by_number(request, month):
