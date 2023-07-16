70939e1ee35341a593f05c4319d576a15521b4d2
SifatNiloy <sifatniloy18@gmail.com>
2023-07-16 17:38:37 +0600

    added some css style

--- monthly_challenges/asgi.py
+++ monthly_challenges/asgi.py
@@ -14,3 +14,12 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'monthly_challenges.settings')
 
 application = get_asgi_application()
+
+
+
+
+
+
+
+
+
