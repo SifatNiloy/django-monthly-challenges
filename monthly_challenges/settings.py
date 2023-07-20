1da2134b84a837b623c24f3ec6e128026e9959ec
SifatNiloy <sifatniloy18@gmail.com>
2023-07-20 21:12:29 +0600

    changed settings

--- monthly_challenges/settings.py
+++ monthly_challenges/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$fnt0w79tqp-=p008it$+hv)+4aip$x^95uo7xkn20p1db11-h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    sifatniloy.pythonanywhere.com, 
+]
 
 
 # Application definition
